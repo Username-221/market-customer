1d869c9a017ba8c3071b5cc9d45483c8533edea1
konstantin <karchevskiykonst@gmail.com>
2020-05-30 17:05:36 +0300

    Update schema mapping

--- src/model/schema-mapping.js
+++ src/model/schema-mapping.js
@@ -4,14 +4,9 @@ const productSchema = require('./schemas/product');
 const userSchema = require('./schemas/user');
 const refreshTokenSchema = require('./schemas/refresh-token');
 
-const OrderModel = model('Order', orderSchema);
-const ProductModel = model('Product', productSchema);
-const UserModel = model('User', userSchema);
-const RefreshTokenModel = model('RefreshToken', refreshTokenSchema);
-
 module.exports = {
-  OrderModel,
-  ProductModel,
-  UserModel,
-  RefreshTokenModel,
+  OrderModel: model('Order', orderSchema),
+  ProductModel: model('Product', productSchema),
+  UserModel: model('User', userSchema),
+  RefreshTokenModel: model('RefreshToken', refreshTokenSchema),
 };
